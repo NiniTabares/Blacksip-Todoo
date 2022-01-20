f655cdd785122b01f47d65862f1136430368e1ab
FelipeNavasTodoo <ln@todoo.co>
2022-01-19 23:57:52 -0500

    [ADD] reporte facturacion electronica peru

---
+++ reports_account_move/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Reports account move',
+
+    'version': '13',
+
+    'author': "Todoo SAS",
+
+    'contributors': ['Luis Felipe Navas ln@todoo.co'],
+
+    'website': "www.todoo.co",
+
+    'category': 'Reports',
+
+    'depends': [
+        'account',
+        'account_move',
+    ],
+
+    'data': [       
+        'reports/report_electronic_invoice_peru.xml',                   
+    ],
+    'installable': True
+}
